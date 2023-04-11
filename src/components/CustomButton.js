611e619b1450c7f5710494c366ed7a717dd84050
GuilhermeSimbalista <guilherme.simbalista@yahoo.com.br>
2023-04-10 22:01:45 -0300

    Definição de estilos da tela de login

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 
-export default function customButton({ label, onPress }) {
+export default function CustomButton({ label, onPress }) {
     return (
         <TouchableOpacity
             onPress={ onPress }
